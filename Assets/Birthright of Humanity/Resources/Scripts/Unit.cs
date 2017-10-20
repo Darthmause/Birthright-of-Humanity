390b9381e893b0d57099aab9b8ec336f1b295a7e
Gabriel Gaspar <darthmouse02@gmail.com>
2017-10-20 17:06:21 -0200

    Created the unit controller system

---
+++ Assets/Birthright of Humanity/Resources/Scripts/Unit.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System;
+
+namespace GabrielGaspar.BirthrightOfHumanity
+{
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(NavMeshAgent))]
+	public class Unit : MonoBehaviour, IDamageable<int>, IMoveable
+	{
+		private Animator animator;
+		private NavMeshAgent agent;
+		private bool selected;
+
+		public Animator Animator
+		{
+			get
+			{
+				return animator;
+			}
+
+			set
+			{
+				animator = value;
+			}
+		}
+
+		public NavMeshAgent Agent
+		{
+			get
+			{
+				return agent;
+			}
+
+			set
+			{
+				agent = value;
+			}
+		}
+
+		public bool Selected
+		{
+			get
+			{
+				return selected;
+			}
+
+			set
+			{
+				selected = value;
+			}
+		}
+
+		public void Start()
+		{
+			UnitController.AddUnit(this);
+		}
+
+		public void Damage(int amount)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Move(Vector3 position)
+		{
+		
+		}
+	}
+}
---
+++ Assets/Birthright of Humanity/Resources/Scripts/UnitController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GabrielGaspar.BirthrightOfHumanity
+{
+	public class UnitController : MonoBehaviour
+	{
+		private Vector2 startSelectionPosition;
+
+		private Vector2 releaseSelectionPosition;
+
+		[SerializeField]
+		public Texture2D selectionMarqueeTexture;
+
+		[SerializeField]
+		public static List<Unit> playerUnits = new List<Unit>();
+
+		[SerializeField]
+		public Unit[] selectedUnits;
+
+		public static void AddUnit(Unit unit)
+		{
+			playerUnits.Add(unit);
+		}
+
+		private void OnGUI()
+		{
+			if (Input.GetButtonDown("Select"))
+			{
+				startSelectionPosition = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+			}
+
+			if (Input.GetButton("Select"))
+			{
+				releaseSelectionPosition = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+				GUI.DrawTexture(new Rect(startSelectionPosition.x, startSelectionPosition.y, releaseSelectionPosition.x - startSelectionPosition.x, releaseSelectionPosition.y - startSelectionPosition.y), selectionMarqueeTexture);
+			}
+
+			if (Input.GetButtonUp("Select"))
+			{
+				foreach (Unit unit in selectedUnits)
+				{
+					unit.Selected = false;
+				}
+
+				Vector2 selectionRetanglePosition = new Vector2() { x = Mathf.Min(startSelectionPosition.x, releaseSelectionPosition.x), y = Mathf.Min(startSelectionPosition.y, releaseSelectionPosition.y) };
+				Vector2 selectionRetangleSize = new Vector2() { x = Mathf.Abs(startSelectionPosition.x - releaseSelectionPosition.x), y = Mathf.Abs(startSelectionPosition.y - releaseSelectionPosition.y) };
+
+				selectedUnits = GetUnitsInSelectionRectangle(new Rect(selectionRetanglePosition, selectionRetangleSize));
+
+				foreach (Unit unit in selectedUnits)
+				{
+					unit.Selected = true;
+				}
+			}
+		}
+
+		public Unit[] GetUnitsInSelectionRectangle(Rect selectionRectangle)
+		{
+			List<Unit> selectedUnits = new List<Unit>();
+
+			foreach (Unit unit in playerUnits)
+			{
+				Vector3 unitPositionInWorld = Camera.main.WorldToScreenPoint(unit.transform.position);
+				Vector2 unitPositionInScreen = new Vector2(unitPositionInWorld.x, Screen.height - unitPositionInWorld.y);
+				if (selectionRectangle.Contains(unitPositionInScreen))
+				{
+					selectedUnits.Add(unit);
+				}
+			}
+			return selectedUnits.ToArray();
+		}
+	}
+}
